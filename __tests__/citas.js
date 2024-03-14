1b12d7169d80be6fb9ebd1a187f34692dd1854d8
Juan Sebastian Giraldo <juans.giraldo.e@gmail.com>
2024-03-14 17:54:58 -0500

    [Learn] - Testing features to edit and delete. (pet-appointment-manager).

--- __tests__/citas.js
+++ __tests__/citas.js
@@ -3,6 +3,7 @@ import Citas from "../js/classes/Citas";
 describe('Probar la clase de citas', () => {
   
   const citas = new Citas();
+  const id= Date.now();
 
   test('Agregar una nueva cita', () => {
     const citaObj = {
@@ -13,10 +14,31 @@ describe('Probar la clase de citas', () => {
       hora:'10:30',
       sintomas: 'Come mucho.'
     };
-    citaObj.id = Date.now();
+    citaObj.id = id;
     citas.agregarCita(citaObj);
 
     //Test.
+    expect(citas).toMatchSnapshot();
+  });
+
+  test('Actualizar cita', () => {
+    const citaActualizada = {
+      id,
+      mascota: 'Honey velez',
+      propietario: 'Melissa',
+      telefono: '31055',
+      fecha: '14-05-2024',
+      hora:'08:30',
+      sintomas: 'Come y duerme mucho.'
+    };
+    citas.editarCita(citaActualizada);
+
+    expect(citas).toMatchSnapshot();
+  });
+
+  test('Eliminar cita', () => {
+    citas.eliminarCita(id);
+
     expect(citas).toMatchSnapshot();
   });
 });
